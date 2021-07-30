c20b4276bc3006effd6825af360d5b47c2f9e5a4
MBSN91 <hello@musabaysan.nl>
2021-07-30 21:36:34 +0200

    opschonen code

--- buttons.js
+++ buttons.js
@@ -1,33 +1,30 @@
-//*** button 1 */
+//*** button test 1 */
 
-$('#1').on('focus', function() {
-    $('#1').addClass('focus');
-    $('#1').removeClass('mouseover');
-    $('#1place').addClass('focus');
-    $('#1place').removeClass('mouseover');
+$("#1").on("focus", function () {
+  $("#1").addClass("focus");
+  $("#1").removeClass("mouseover");
+  $("#1place").addClass("focus");
+  $("#1place").removeClass("mouseover");
 });
 
-$('#1').on('mouseover', function() {
-    $('#1').removeClass('focus');
-    $('#1').addClass('mouseover');
-    $('#1place').removeClass('focus');
-    $('#1place').addClass('mouseover');
+$("#1").on("mouseover", function () {
+  $("#1").removeClass("focus");
+  $("#1").addClass("mouseover");
+  $("#1place").removeClass("focus");
+  $("#1place").addClass("mouseover");
 });
 
-
-$('#1').on('mouseout', function() {
-        $('#1').removeClass('focus');
-        $('#1').removeClass('mouseover');
-        $('#1place').removeClass('focus');
-        $('#1place').removeClass('mouseover');    
-    });
-
-$('#1').click(function(e) {
-		$('#1').addClass('focus');
-    $('#1').removeClass('mouseover');
-    $('#1place').addClass('focus');
-    $('#1place').removeClass('mouseover');
-    $('#1').css('pointer-events', 'none');
+$("#1").on("mouseout", function () {
+  $("#1").removeClass("focus");
+  $("#1").removeClass("mouseover");
+  $("#1place").removeClass("focus");
+  $("#1place").removeClass("mouseover");
 });
 
-
+$("#1").click(function (e) {
+  $("#1").addClass("focus");
+  $("#1").removeClass("mouseover");
+  $("#1place").addClass("focus");
+  $("#1place").removeClass("mouseover");
+  $("#1").css("pointer-events", "none");
+});
